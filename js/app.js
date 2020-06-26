7cd22a57222ff7f5822748ad5767cde289c3a5cd
patricialan <54603331+patricialan@users.noreply.github.com>
2020-06-26 14:57:28 -0600

    finished app.js

--- js/app.js
+++ js/app.js
@@ -23,5 +23,29 @@ function buildTable(data) {
             }
         );
     });
-};
+}
 
+// Function to handle button click (to filter table)
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    // Set default table data to be raw data
+    let filteredData = tableData;
+
+    // Check if a date was entered and filter the data using that date
+    if (date) {
+        // Apply filter to the table data to only keep rows where
+        // 'datetime' value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData
+    // will just be the original tableData
+    buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handClick);
+
+// Build the table when the page loads
+buildTable(tableData);
